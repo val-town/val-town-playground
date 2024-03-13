c68e0f2f14fdfadc7cd80ea1661eaca0974f1635
pomdtr <achille.lacoin@gmail.com>
2024-03-13 18:39:20 +0100

    remove comment on top

--- src/playground.ts
+++ src/playground.ts
@@ -1,21 +1,3 @@
-// import {LitElement, html, TemplateResult} from 'lit';
-// import styles from './hello-world.css' assert {type: 'css'};
-
-// export class HelloWorldElement extends LitElement {
-//   public static styles = [styles];
-//   public render(): TemplateResult {
-//     return html` <h1 class="text-3xl font-bold underline">Hello world!</h1> `;
-//   }
-// }
-
-// window.customElements.define('hello-world', HelloWorldElement);
-
-/**
- * @license
- * Copyright 2019 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
 import {LitElement, html, nothing} from 'lit';
 import styles from './playground.css' assert {type: 'css'};
 import {customElement, property, state} from 'lit/decorators.js';
