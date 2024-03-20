153bbb522447784b32bade85c6c7df571bb95986
pomdtr <achille.lacoin@gmail.com>
2024-03-20 16:24:06 +0100

    improve UI (add loading indicator, improve overflow)

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 export default defineConfig({
   build: {
     lib: {
-      entry: 'src/vt-playground.ts',
+      entry: ['src/vt-playground.ts'],
       formats: ['es']
     }
   },
